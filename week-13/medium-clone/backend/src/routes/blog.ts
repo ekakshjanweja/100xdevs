b59fa4077abfefe3e89797f7f00e979aeee565d0
ekaksh <jekaksh@gmail.com>
2024-03-02 17:34:08 +0530

    Week 13: common npm package published

--- week-13/medium-clone/backend/src/routes/blog.ts
+++ week-13/medium-clone/backend/src/routes/blog.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { createBlogInput, updateBlogInput } from "@stormej/common-app";
 import { Hono } from "hono";
 import { verify } from "hono/jwt";
 
@@ -43,6 +44,13 @@ blogRouter.post("/", async (c) => {
 
   const body = await c.req.json();
 
+  const { success } = createBlogInput.safeParse(body);
+
+  if (!success) {
+    c.status(400);
+    return c.json({ error: "invalid input" });
+  }
+
   const userId = c.get("userId");
 
   const post = await prisma.post.create({
@@ -56,8 +64,31 @@ blogRouter.post("/", async (c) => {
   return c.json({ id: post.id });
 });
 
-blogRouter.put("/api/v1/blog", (c) => {
-  return c.text("blog route");
+blogRouter.put("/api/v1/blog", async (c) => {
+  const userId = c.get("userId");
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  const { success } = updateBlogInput.safeParse(body);
+  if (!success) {
+    c.status(400);
+    return c.json({ error: "invalid input" });
+  }
+
+  prisma.post.update({
+    where: {
+      id: body.id,
+      authorId: userId,
+    },
+    data: {
+      title: body.title,
+      content: body.content,
+    },
+  });
+
+  return c.text("updated post");
 });
 
 blogRouter.get("/:id", async (c) => {
--- week-13/medium-clone/backend/src/routes/user.ts
+++ week-13/medium-clone/backend/src/routes/user.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
 import { sign } from "hono/jwt";
+import { signInInput, signUpInput } from "@stormej/common-app";
 
 export const userRouter = new Hono<{
   Bindings: {
@@ -16,6 +17,13 @@ userRouter.post("/signup", async (c) => {
   }).$extends(withAccelerate());
 
   const body = await c.req.json();
+
+  const { success } = signUpInput.safeParse(body);
+
+  if (!success) {
+    c.status(400);
+    return c.json({ error: "Invalid Input" });
+  }
 
   try {
     const user = await prisma.user.create({
@@ -44,6 +52,13 @@ userRouter.post("/signin", async (c) => {
   }).$extends(withAccelerate());
 
   const body = await c.req.json();
+
+  const { success } = signInInput.safeParse(body);
+
+  if (!success) {
+    c.status(400);
+    return c.json({ error: "Invalid Input" });
+  }
 
   const user = await prisma.user.findUnique({
     where: {
